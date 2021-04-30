e6d7fd2d0e44b20424d9f94f20a6279f1e778f64
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2021-04-30 17:18:04 -0400

    Comment unnecesary line

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from core import *
 from strings import *
 
 token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
+#some_api_token = os.environ['SOME_API_TOKEN']
 
 # If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
 #r = redis.from_url(os.environ.get("REDIS_URL"))
