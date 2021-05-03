b20b1e1573ccd863f5cae9250815dad53ac51377
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2021-05-03 15:53:42 -0400

    Update strings.py

--- pygmentbot/strings.py
+++ pygmentbot/strings.py
@@ -1,18 +1,18 @@
 str_tips = """\
 _No envíes mensajes demasiado largos_
 Recuerda que yo te devuelvo el código en formato de imagen, por lo \
-tanto, mientras más largo el código, más se expande la imagen y menos se \
-ve el contenido, por ahora funciono mejor con snippets, pero quizás mi Papá \
+tadnto, mientras más largo el código, más se expande la imagen y menos se \
+ve el contenido, por ahora funciono mejor con snippets, pero quizás mi Padre \
 mejore eso más adelante.
 
 _Esquema de colores_
 El esquema de colores por defecto del código es \
 [Dracula](https://draculatheme.com/pygments), si te gustaría poder \
-elegir entre otros temas, por favor repórtalo a mi Papá :3
+elegir entre otros temas, por favor repórtalo a mi Padre :3
 """
 
 str_info = """\
-*Versión actual:* 0.3
+*Versión actual:* 0.4
 *Desarrollador:* Pedro Torcatt
 *Página oficial:* https://jalkhov.github.io/pygment-bot/
 *Código fuente:* https://github.com/Jalkhov/pygment-bot
@@ -21,7 +21,8 @@ str_info = """\
 str_selexer = """\
 Ok, elige el lenguaje de programación que deseas utilizar o escribe el\
 código exacto de algún otro lenguaje, para ver el listado completo de códigos\
-accede al siguiente [enlace](https://jalkhov.github.io/pygment-bot/#lenguajes_soportados)
+accede al siguiente [enlace](https://jalkhov.github.io/pygment-bot) en el\
+apartado "Lenguajes soportados".
 """
 
 availangs = {'Python': 'python',
